fcea200c9b2a8d9f426d4b9f5e53b2d6e09130df
nguyengiabao91753 <140630293+nguyengiabao91753@users.noreply.github.com>
2024-09-10 21:30:04 +0700

    push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { VehicleTypeComponent } from './admin/component/vehicle-type/vehicle-type.component';
 import { AgegroupComponent } from './admin/component/agegroup/agegroup.component';
 
 export const routes: Routes = [
@@ -8,10 +7,6 @@ export const routes: Routes = [
         path: '',
         component: AdminComponent,
         children:[
-            {
-                path:'table',
-                component: VehicleTypeComponent
-            },
             {
                 path:'agegroup',
                 component: AgegroupComponent
