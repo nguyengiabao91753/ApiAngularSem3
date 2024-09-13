98940ef82b23f162718ceb370066f6a4144ad4f6
Er2g0n <hnguyenngoc.h@gmail.com>
2024-09-13 14:59:45 +0700

    hai pull

---
+++ src/app/admin/auth/create/create.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  providers: [MessageService],
+  imports: [
+    CommonModule,
+    TableModule,
+    FileUploadModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    RippleModule,
+    ToastModule,
+    ToolbarModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    RadioButtonModule,
+    InputNumberModule,
+    DialogModule,
+    RatingModule
+  ],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css'
+})
+export class CreateComponent implements OnInit{
+
+  
+  ngOnInit(): void {
+  }
+
+}
